15e4482a4ffa3215285ca75f8310f7d578756274
Tyrannas <someonefefe@gmail.com>
2023-01-18 23:13:41 +0100

    changing dev.sh

--- dev.sh
+++ dev.sh
@@ -1,11 +1,11 @@
 cd src/
 if [ ! -d "./data" ];
 then
-    git clone https://github.com/Amleth/ceres-www-content.git ./data
+    git clone https://github.com/CERES-Sorbonne/ceres-www-content.git ./data
 fi
 cd ./data
 git pull origin main
-curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th358&format=jsonld" -o thesaurus.json 
+# curl "https://opentheso.huma-num.fr/opentheso/api/all/theso?id=th358&format=jsonld" -o thesaurus.json 
 cd ../..
 
 npx gatsby clean
